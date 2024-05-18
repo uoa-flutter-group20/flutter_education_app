2b6b4bdfd57bf032057ac917fb2232c5e5c409eb
gokce <gaia.gkce@gmail.com>
2024-05-18 13:22:18 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:oua_flutter_twenty/presentation/screens/onboarding_screen.dart';
+
+
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Onboarding Screen',
+      home: OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/presentation/screens/signin_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return const Scaffold();
+
+  }
+}
