44f86e0f604ebdde81c537da2728f888cc788793
gokce <gaia.gkce@gmail.com>
2024-05-21 14:21:07 +0300

    d

--- lib/presentation/screens/signin_page.dart
+++ lib/presentation/screens/signin_page.dart
@@ -10,10 +10,10 @@ class SignIn extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign In'),
+        title: const Text('Sign In '),
       ),
       body: Center(
-        child: ElevatedButton(
+        child: ElevatedButton (
           onPressed: () {
             Navigator.push(
               context,
